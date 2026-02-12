3427071133640930d846cde62bda704c74c019b1
P.Makarchuk@creatio.com <P.Makarchuk@creatio.com>
2026-02-12 13:48:42 +0200

    Changed TargetFramework from net9.0 to net8.0 and added using

--- ConsoleGit/ConsoleGit/Program.cs
+++ ConsoleGit/ConsoleGit/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using ConsoleGit.Commands;
 using ConsoleGit.ConfiguredHttpClient;
 using ConsoleGit.Services;
--- packages/AtfTIDE/Schemas/AtfProcessUserTask_GitAddSome/AtfProcessUserTask_GitAddSome.cs
+++ packages/AtfTIDE/Schemas/AtfProcessUserTask_GitAddSome/AtfProcessUserTask_GitAddSome.cs
@@ -1,74 +1,74 @@
-using AtfTIDE;
-using Terrasoft.Core.Factories;
-
-namespace Terrasoft.Core.Process.Configuration
-{
-
-	using System;
-	using System.Collections.Generic;
-	using System.Collections.ObjectModel;
-	using System.Globalization;
-	using Terrasoft.Common;
-	using Terrasoft.Core;
-	using Terrasoft.Core.Configuration;
-	using Terrasoft.Core.DB;
-	using Terrasoft.Core.Entities;
-	using Terrasoft.Core.Process;
-
-	#region Class: AtfProcessUserTask_GitAddSome
-
-	/// <exclude/>
-	public partial class AtfProcessUserTask_GitAddSome
-	{
-
-		#region Methods: Protected
-
-		protected override bool InternalExecute(ProcessExecutingContext context) {
-			string repositoryName = HelperFunctions.FetchRepositoryName(Repository, UserConnection);
-			ConsoleGitArgs args = new ConsoleGitArgs {
-				Command = AtfTIDE.Commands.AddSome,
-				RepoDir = HelperFunctions.GetRepositoryDirectory(repositoryName).ToString(),
-				Files = Files
-			};
-			
-			ConsoleGitResult gitCommandResult = ClassFactory
-												.Get<IConsoleGit>("AtfTIDE.ConsoleGit")
-												.Execute(args);
-			
-			if(gitCommandResult.ExitCode != 0) {
-				IsError = true;
-				ErrorMessage = gitCommandResult.ErrorMessage;
-			}else {
-				Output = gitCommandResult.Output;
-			}
-			return true;
-		}
-
-		#endregion
-
-		#region Methods: Public
-
-		public override bool CompleteExecuting(params object[] parameters) {
-			return base.CompleteExecuting(parameters);
-		}
-
-		public override void CancelExecuting(params object[] parameters) {
-			base.CancelExecuting(parameters);
-		}
-
-		public override string GetExecutionData() {
-			return string.Empty;
-		}
-
-		public override ProcessElementNotification GetNotificationData() {
-			return base.GetNotificationData();
-		}
-
-		#endregion
-
-	}
-
-	#endregion
-
-}
-
+// using AtfTIDE;
+// using Terrasoft.Core.Factories;
+//
+// namespace Terrasoft.Core.Process.Configuration
+// {
+//
+// 	using System;
+// 	using System.Collections.Generic;
+// 	using System.Collections.ObjectModel;
+// 	using System.Globalization;
+// 	using Terrasoft.Common;
+// 	using Terrasoft.Core;
+// 	using Terrasoft.Core.Configuration;
+// 	using Terrasoft.Core.DB;
+// 	using Terrasoft.Core.Entities;
+// 	using Terrasoft.Core.Process;
+//
+// 	#region Class: AtfProcessUserTask_GitAddSome
+//
+// 	/// <exclude/>
+// 	public partial class AtfProcessUserTask_GitAddSome
+// 	{
+//
+// 		#region Methods: Protected
+//
+// 		protected override bool InternalExecute(ProcessExecutingContext context) {
+// 			string repositoryName = HelperFunctions.FetchRepositoryName(Repository, UserConnection);
+// 			ConsoleGitArgs args = new ConsoleGitArgs {
+// 				Command = AtfTIDE.Commands.AddSome,
+// 				RepoDir = HelperFunctions.GetRepositoryDirectory(repositoryName).ToString(),
+// 				Files = Files
+// 			};
+// 			
+// 			ConsoleGitResult gitCommandResult = ClassFactory
+// 												.Get<IConsoleGit>("AtfTIDE.ConsoleGit")
+// 												.Execute(args);
+// 			
+// 			if(gitCommandResult.ExitCode != 0) {
+// 				IsError = true;
+// 				ErrorMessage = gitCommandResult.ErrorMessage;
+// 			}else {
+// 				Output = gitCommandResult.Output;
+// 			}
+// 			return true;
+// 		}
+//
+// 		#endregion
+//
+// 		#region Methods: Public
+//
+// 		public override bool CompleteExecuting(params object[] parameters) {
+// 			return base.CompleteExecuting(parameters);
+// 		}
+//
+// 		public override void CancelExecuting(params object[] parameters) {
+// 			base.CancelExecuting(parameters);
+// 		}
+//
+// 		public override string GetExecutionData() {
+// 			return string.Empty;
+// 		}
+//
+// 		public override ProcessElementNotification GetNotificationData() {
+// 			return base.GetNotificationData();
+// 		}
+//
+// 		#endregion
+//
+// 	}
+//
+// 	#endregion
+//
+// }
+//
